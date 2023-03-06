4a534b9692b20f93f111c65cf5cfb436b87c2023
AlexisleoMena <alexismena2690@gmail.com>
2023-03-06 18:19:56 -0300

    Cargamos los middlewares

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const errorHandler = require("./utils/middlewares/errorHandler");
+const setHeaders = require("./utils/middlewares/setHeaders");
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const app = express();
+
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(morgan("dev"));
+app.use(setHeaders);
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+app.use(errorHandler);
 
 module.exports = app;
