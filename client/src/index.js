73f26fc00553713a6653c3eaffa58f605900c3ee
AlexisleoMena <alexismena2690@gmail.com>
2023-03-06 21:10:18 -0300

    Hicimos deploy sin server

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,8 @@ import axios from "axios";
 import App from './App';
 import './index.css';
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+// axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL = process.env.REACT_APP_API || "https://fakestoreapi.com";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
